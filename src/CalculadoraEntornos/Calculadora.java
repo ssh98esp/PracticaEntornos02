de7ce36fb14706fb732e4905f51bf6720508b340
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-08 13:06:15 +0100

    Commit mMultiplicar Static

--- src/CalculadoraEntornos/Calculadora.java
+++ src/CalculadoraEntornos/Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
         return num1 - num2;
     }
 
-    public double multiplicar(double num1, double num2) {
+    public static double multiplicar(double num1, double num2) {
         return num1 * num2;
     }
 
