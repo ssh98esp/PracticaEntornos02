7a1c70344edc8a08454e4d8aa0083bbb0ae30bb6
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-08 13:07:59 +0100

    Commit tSumar

--- Test/CalculadoraEntornos/CalculadoraTest.java
+++ Test/CalculadoraEntornos/CalculadoraTest.java
@@ -3,6 +3,8 @@ package CalculadoraEntornos;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +18,12 @@ class CalculadoraTest {
     void tearDown() {
     }
 
-
+    @ParameterizedTest(name = "La suma de {0} y {1} es {2}")
+    @CsvSource({"5, 5, 10",
+            "10, 15, 25",
+            "4, 3, 7"})
+    void sumar(double num1, double num2, double expectedResult){
+        assertEquals(expectedResult,
+                Calculadora.sumar(num1,num2), 0.1);
+    }
 }
