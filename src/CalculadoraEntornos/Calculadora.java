58576fe481aa7496e2a663fcdd65e688b529bc0e
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-08 13:06:56 +0100

    Commit mDividir Static

--- src/CalculadoraEntornos/Calculadora.java
+++ src/CalculadoraEntornos/Calculadora.java
@@ -13,7 +13,7 @@ public class Calculadora {
         return num1 * num2;
     }
 
-    public double dividir(double num1, double num2) {
+    public static double dividir(double num1, double num2) {
         if (num2 == 0) {
             return 0;
         } else {
