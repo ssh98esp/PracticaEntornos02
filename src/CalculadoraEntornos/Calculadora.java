73da0e9c749b346388196326602647aa67f9989e
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-08 13:05:29 +0100

    Commit mRestar Static

--- src/CalculadoraEntornos/Calculadora.java
+++ src/CalculadoraEntornos/Calculadora.java
@@ -5,7 +5,7 @@ public class Calculadora {
         return num1 + num2;
     }
 
-    public double restar(double num1, double num2) {
+    public static double restar(double num1, double num2) {
         return num1 - num2;
     }
 
