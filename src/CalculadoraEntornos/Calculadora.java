dd34f594d7ae7a906d73f67d8d1c948f910c3619
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-08 13:04:21 +0100

    Commit mSumar Static

--- src/CalculadoraEntornos/Calculadora.java
+++ src/CalculadoraEntornos/Calculadora.java
@@ -1,7 +1,7 @@
 package CalculadoraEntornos;
 
 public class Calculadora {
-    public double sumar(double num1, double num2) {
+    public static double sumar(double num1, double num2) {
         return num1 + num2;
     }
 
