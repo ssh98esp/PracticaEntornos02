c5b4234491aef17f785264eae923882edcdc0fac
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-08 12:53:30 +0100

    Commit mDividir

--- src/CalculadoraEntornos/Calculadora.java
+++ src/CalculadoraEntornos/Calculadora.java
@@ -12,4 +12,12 @@ public class Calculadora {
     public double multiplicar(double num1, double num2) {
         return num1 * num2;
     }
+
+    public double dividir(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+    }
 }
