8a3134b51d1debd3bba86da8e446b83303c751a3
Samuel Salguero <ssalgueroherrero@gmail.com>
2024-03-09 15:25:00 +0100

    Commit Coment5

--- src/CalculadoraEntornos/Calculadora.java
+++ src/CalculadoraEntornos/Calculadora.java
@@ -35,6 +35,13 @@ public class Calculadora {
         return num1 * num2;
     }
 
+    /**
+     * El metodo devuelve un double como resultado de la división de los
+     * parámetros que le facilitamos.
+     * @param num1 Primer número de la operación
+     * @param num2 Segundo número de la operación
+     * @return Devuelve el resultado de la operación
+     */
     public static double dividir(double num1, double num2) {
         if (num2 == 0) {
             return 0;
